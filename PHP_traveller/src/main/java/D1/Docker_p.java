1f0f99f94bd8834f62faa89f386c06d4a810bfb1
deepshikharai <rdeepshikha502@gmail.com>
2018-05-29 12:15:05 +0530

    comment this the first test added

--- PHP_traveller/src/main/java/D1/Docker_p.java
+++ PHP_traveller/src/main/java/D1/Docker_p.java
@@ -80,6 +80,7 @@ public class Docker_p {
 	
 	@Test
 	public void test1() throws Exception {
+		//this is the first test
 		WebDriverWait wait = new WebDriverWait(driver,1000);
 		//Thread.sleep(1000);
 		//Thread.sleep(1000);
