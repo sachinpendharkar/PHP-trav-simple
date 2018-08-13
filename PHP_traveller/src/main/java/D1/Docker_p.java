f27dac4c82154989853f757e264f5f7fccf2d71a
pramila-git <36467622+pramila-git@users.noreply.github.com>
2018-08-13 12:35:28 +0530

    Changing selenium url to zalenium

--- PHP_traveller/src/main/java/D1/Docker_p.java
+++ PHP_traveller/src/main/java/D1/Docker_p.java
@@ -46,7 +46,7 @@ public class Docker_p {
 		
 		dcp.setCapability("idleTimeout", 150);
 		
-		driver = new RemoteWebDriver(new URL("http://35.202.120.148:4444/wd/hub"),dcp);
+		driver = new RemoteWebDriver(new URL("http://104.155.135.94:4444/wd/hub"),dcp);
 	
 		/*System.setProperty("webdriver.chrome.driver","D:\\drivers\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver(); 
